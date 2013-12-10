5c914fba6a2179bd1f1ec57a199d271f12c35870
Marco Kretz <zantekk@gmail.com>
2013-12-10 13:58:27 +0100

    First.

---
+++ adb_util.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import re
+import sys
+import subprocess
+from os.path import expanduser, isfile
+
+
+ADB_DEV_PATTERN = re.compile("^[A-Z0-9]{12}\sdevice$")
+ADB_EXE = expanduser("~") + "/.andropy/bin/adb"
+
+class ADBInterface():
+    """
+    Simple interface for working with ADB.
+    """
+    def __init__(self):
+        if not isfile(ADB_EXE):
+            sys.exit("No ADB binary has been found.\n" + \
+                     "Please run the Installer first.")
+        subprocess.check_output([ADB_EXE, "start-server"])
+
+    def __enter__(self):
+        if len(self.get_devices()) > 0:
+            return self
+        sys.exit("No devices found.")
+
+    def get_devices(self):
+        """
+        Return a list containing all connected ADB-devices.
+        """
+        devices = []
+        out = subprocess.check_output([ADB_EXE, "devices"],
+                                      universal_newlines=True)
+        self.check_output(out)
+        for line in out.split('\n'):
+            if ADB_DEV_PATTERN.match(line):
+                devices.append(line[:12])
+        return devices
+
+    def exec_shell_cmd(self, cmd, output=True):
+        """
+        Exec a shell command via ADB, select wether output is returned or not.
+        Return a list of output-lines.
+        """
+        response = []
+        out = subprocess.check_output([ADB_EXE, "shell", cmd],
+                                      universal_newlines=True)
+        self.check_output(out)
+        for line in out.split('\n'):
+            response.append(line)
+        return response
+
+    def get_build_props(self):
+        """
+        Get contents of /system/build.prop as dict.
+        Return dictionary like: Property => Value
+        """
+        build_props = {}
+        out = subprocess.check_output([ADB_EXE, "shell",
+                                      "cat /system/build.prop"],
+                                      universal_newlines=True)
+        self.check_output(out)
+        for line in out.split('\n'):
+            if line == "":
+                continue
+            prop = line.split('=')
+            if not prop[0].startswith('#'):
+                build_props[prop[0]] = prop[1]
+        return build_props
+
+
+    def reboot(self, option):
+        if option == "system":
+            subprocess.call([ADB_EXE, "reboot"])
+        elif option in ("recovery", "bootloader"):
+            subprocess.call([ADB_EXE, "reboot", option])
+
+
+    def check_output(self, out):
+        """
+        Check if the response of a command is valid.
+        """
+        if out == "error: device not found":
+            sys.exit("Error connecting to device!")
+
+
+    def __exit__(self, type, value, traceback):
+        subprocess.call([ADB_EXE, "kill-server"])
+
+
+with ADBInterface() as adbi:
+    bp = adbi.get_build_props()
+    print(bp['net.bt.name'])
---
+++ installer.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import platform
+import os
+import sys
+import urllib.request as urll
+from os.path import expanduser
+
+
+adb_link = "https://github.com/tehmk/androgola-platform-tools/raw/master/"
+
+class Installer():
+    def __init__(self, adb, fastboot):
+        self.adb = adb
+        self.fastboot = fastboot
+        self.arch = platform.architecture()[0]
+        self.os = platform.system().lower()
+        self.homedir = expanduser("~")
+
+    def execute(self):
+        def create_download_url(tool):
+            if tool in ("adb", "fastboot"):
+                return adb_link + self.os + "/" + tool + self.arch[:2]
+
+        adb_dl = create_download_url("adb")
+        fb_dl = create_download_url("fastboot")
+        try:
+            os.makedirs(self.homedir + "/.andropy/bin", exist_ok=True)
+            if self.adb:
+                urll.urlretrieve(adb_dl, self.homedir+"/.andropy/bin/adb")
+            if self.fastboot:
+                urll.urlretrieve(fbd_l, self.homedir+"/.andropy/bin/fastboot")
+        except OSError:
+            sys.exit("Folder creation failed!")
+
+
+
+
+i = Installer(True, False)
+print("ADB:", i.adb)
+print("Fastboot:", i.fastboot)
+print("Arch:", i.arch)
+print("OS:", i.os)
+print("Location:", i.homedir)
+
+i.execute()
